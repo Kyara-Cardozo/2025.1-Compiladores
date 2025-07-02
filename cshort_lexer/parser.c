8dc651b9f308edd07b42e8ecdde8c19120a53aa2
adeonita <adeonita.sousa@gmail.com>
2025-07-02 00:02:21 -0300

    refactor: refactor decl kind function to handle with table symbol

--- cshort_lexer/parser.c
+++ cshort_lexer/parser.c
@@ -114,21 +114,25 @@ static void parseBloco()
     match(FECHACHAVE);
 }
 
-// < tipos_param> ::= <tipo> ID ( parametros );
-static void reconheceParametro()
-{
-    if (t.type != ID) {
-        printf("Erro de sintaxe na linha %d: identificador esperado após tipo.\n", t.line);
-        exit(1);
-    }
 
-    advanceToken();
-    return;
-}
+// < tipos_param> ::= <tipo> ID ( parametros );
+// static void reconheceParametro(const char* nome)
+// {
+//     if (t.type != ID) {
+//         printf("Erro de sintaxe na linha %d: identificador esperado após tipo.\n", t.line);
+//         exit(1);
+//     }
+
+//     advanceToken();
+//     return;
+// }
 
 // <decl> ::= <tipo> ID ( ) | <tipo> ID ;
 static DeclKind decl()
 {
+    char nome[256];
+    strcpy(nome, t.lexeme);
+
     parseTipo();
 
     if (t.type != ID)
@@ -137,32 +141,42 @@ static DeclKind decl()
         exit(1);
     }
 
-    char nome[256];
     strcpy(nome, t.lexeme);
     advanceToken();
 
     if (t.type == ABREPARENTESE)
     {
+        insertSymbol(nome, SYMBOL_FUNC, t.type);
+
         advanceToken();
-        parseTipoSemVoid();
 
-        reconheceParametro();
+        if (t.type != FECHAPARENTESE)
+        {
+            do
+            {
+                parseTipoSemVoid();
 
-        // insertSymbol(nome, SYMBOL_VAR);
+                if (t.type != ID)
+                {
+                    printf("Erro de sintaxe na linha %d: identificador esperado após tipo do parâmetro.\n", t.line);
+                    exit(1);
+                }
 
-        // exit(0);
+                insertSymbol(t.lexeme, SYMBOL_PARAM, t.type);
+                advanceToken();
+
+            } while (t.type == VIRGULA); // Continua enquanto houver mais parâmetros
+        }
+
         match(FECHAPARENTESE);
 
-        if (t.type == ABRECHAVE)
-        // to do reconhecer conteudo dentro da funcao
-        { // t já aponta pro próximo
-            insertSymbol(nome, SYMBOL_FUNC);
-            return DECL_PROT_UNICO; // Indica definição
-        }
-        else if (t.type == PONTOVIRGULA)
+        if (t.type == ABRECHAVE) // Definição da função
         {
-            insertSymbol(nome, SYMBOL_FUNC);
-            return DECL_PROT; // Indica protótipo
+            return DECL_PROT_UNICO;
+        }
+        else if (t.type == PONTOVIRGULA) // Protótipo da função
+        {
+            return DECL_PROT;
         }
         else
         {
@@ -170,24 +184,23 @@ static DeclKind decl()
             exit(1);
         }
     }
-
-    else if (t.type == PONTOVIRGULA)
+    else if (t.type == PONTOVIRGULA) // Caso seja uma variável
     {
-        insertSymbol(nome, SYMBOL_VAR);
+        insertSymbol(nome, SYMBOL_VAR, t.type);
         return DECL_VAR;
     }
-    else if (t.type == VIRGULA)
+    else if (t.type == VIRGULA) // Caso seja uma lista de variáveis
     {
-        insertSymbol(nome, SYMBOL_VAR);
+        insertSymbol(nome, SYMBOL_VAR, t.type);
         do
         {
-            advanceToken(); // Avança para o próximo identificador
+            advanceToken();
             if (t.type != ID)
             {
                 printf("Erro de sintaxe na linha %d: identificador esperado após ','.\n", t.line);
                 exit(1);
             }
-            insertSymbol(t.lexeme, SYMBOL_VAR);
+            insertSymbol(t.lexeme, SYMBOL_VAR, t.type);
             advanceToken();
         } while (t.type == VIRGULA);
 
@@ -203,8 +216,6 @@ static DeclKind decl()
     }
     else
     {
-        printf("aqui else");
-        printf("aqui2 else:%d \n", t.type);
         printf("Erro de sintaxe na linha %d: esperado '(' ou ';' após identificador.\n", t.line);
         exit(1);
     }
